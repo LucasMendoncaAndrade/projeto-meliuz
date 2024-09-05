293d05985dfe226ef6a8449d7a8562e0da18ca5a
Lucas Mendonca Andrade <andrademendoncalucas@gmail.com>
2024-09-05 19:26:19 -0300

    fix: correcao aplicada a funcao get_5_recommendations

--- code/app/services/recommendation_service.py
+++ code/app/services/recommendation_service.py
@@ -12,7 +12,6 @@ def get_5_recommendations(data_path):
     min_daily_income = products_df['daily_income'].min()
     daily_income_bins = np.linspace(min_daily_income - 1e-6, max_daily_income + 1e-6, 6).tolist()
     products_df['daily_income_class'] = pd.cut(products_df['daily_income'], bins=daily_income_bins, labels=[1, 2, 3, 4, 5], right=False)
-    products_df_grouped = products_df.groupby('daily_income_class', observed=False).apply(lambda x: x.sample(1), include_groups=False)
-    products = products_df_grouped.reset_index(drop=True)[['product_title', 'product_price', 'product_image_url', 'store_name']]
-    
+    products_df_grouped = products_df.groupby('daily_income_class', observed=False).apply(lambda x: x[['product_title', 'product_price', 'product_image_url', 'store_name']].sample(1))
+    products = products_df_grouped.reset_index(drop=True)
     return products.to_dict(orient='index')
